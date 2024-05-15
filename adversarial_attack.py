1c9d54dc4293cffe6d549353b6d8f39d476b6e33
アンドリュー <bitweal12@gmail.com>
2024-05-15 23:34:59 +0300

    Renamed fgsm_attack() to bim_attack()
    Added exit from the algorithm when changing the class of an object

--- adversarial_attack.py
+++ adversarial_attack.py
@@ -16,20 +16,13 @@ class AdversarialAttack:
         self.image_in_tensor = None
         self.batch = None
         self.data_grad = None
+        self.predicted_class = None
 
     @staticmethod
     def load_labels(classes_path):
         with open(classes_path, 'r') as f:
             labels = [line.strip() for line in f]
         return labels
-
-    def load_image(self, image_path):
-        self.image = Image.open(image_path)
-        self.image_in_tensor = transforms.ToTensor()(self.image).unsqueeze(0)
-
-    def tensor_to_batch(self, input_tensor):
-        self.batch = input_tensor.unsqueeze(0)
-        return self.batch
 
     @staticmethod
     def save_tensor_image(image_tensor, path_save_image):
@@ -43,6 +36,14 @@ class AdversarialAttack:
         processed_image = image.resize((224, 224), resample=Image.BILINEAR)
         processed_image.save(f'media/{path_save_image}.jpg')
 
+    def load_image(self, image_path):
+        self.image = Image.open(image_path)
+        self.image_in_tensor = transforms.ToTensor()(self.image).unsqueeze(0)
+
+    def tensor_to_batch(self, input_tensor):
+        self.batch = input_tensor.unsqueeze(0)
+        return self.batch
+
     def compute_gradient(self):
         batch = copy.deepcopy(self.batch)
         model = copy.deepcopy(self.model)
@@ -55,24 +56,30 @@ class AdversarialAttack:
         data_grad = batch.grad.data
         self.data_grad = data_grad
 
-    def fgsm_attack(self):
+    def bim_attack(self):
         self.compute_gradient()
+        predicted_class = None
         for eps in np.arange(0, 0.5, 0.01):
-            data_grad = self.data_grad
+            data_grad = copy.deepcopy(self.data_grad)
             sign_data_grad = data_grad.sign()
             perturbed_image = self.image_in_tensor + eps * sign_data_grad
             perturbed_image = torch.clamp(perturbed_image, 0, 1)
-            self.save_tensor_image(perturbed_image, f'test_dog_{eps}')
+            self.save_tensor_image(perturbed_image, f'bim_attack{eps}')
             print(eps)
             image = self.image
             image_in_tensor = self.image_in_tensor
-            self.load_image(f'media/test_dog_{eps}.jpg')
-            self.predict()
+            self.load_image(f'media/bim_attack{eps}.jpg')
+
+            if self.predicted_class is None:
+                self.predicted_class = self.predict()
+            else:
+                predicted_class = self.predict()
+
             self.image = image
             self.image_in_tensor = image_in_tensor
 
-    def bim_attack(self, image, epsilon, data_grad):
-        pass
+            if predicted_class != self.predicted_class:
+                break
 
     def predict(self):
         input_image = self.image
@@ -89,11 +96,15 @@ class AdversarialAttack:
             output = self.model(input_batch)
 
         probabilities = torch.nn.functional.softmax(output[0], dim=0)
-
         predicted_class_idx = torch.argmax(probabilities).item()
         predicted_class = self.class_labels[predicted_class_idx]
+
         print("Predicted class:", predicted_class)
         print("Probability:", probabilities[predicted_class_idx].item())
+
+        if self.predicted_class is None:
+            self.predicted_class = predicted_class
+        return predicted_class
 
 
 if __name__ == "__main__":
@@ -104,4 +115,4 @@ if __name__ == "__main__":
     attack = AdversarialAttack(model, file_classes)
     attack.load_image(filename)
     attack.predict()
-    attack.fgsm_attack()
+    attack.bim_attack()
